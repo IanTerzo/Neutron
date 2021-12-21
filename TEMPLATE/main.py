195a1f09b1db616d6d37223eb0a15a646739896b
Ian Baldelli <79420376+IanTerzo@users.noreply.github.com>
2021-12-21 23:17:03 +0100

    Template project: main.py

---
+++ TEMPLATE/main.py
@@ -0,0 +1,13 @@
+import Neutron
+from Neutron.utils import css
+
+win = Neutron.Window("Example", size=(600,100), css="def.css")
+win.display(file="render.html")
+
+def onClick():
+    win.getElementById("title").innerHTML = "Hello:" + win.getElementById("inputName").value
+
+win.getElementById("submitName").AddEventListener("click", Neutron.event(onClick))
+
+
+win.show()
