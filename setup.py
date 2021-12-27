de939c30a0552d3aba327c684be2a157f75e88da
Ian Baldelli <79420376+IanTerzo@users.noreply.github.com>
2021-12-27 20:08:19 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "PYPI-README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+DESCRIPTION = 'Streaming video data via networks'
+
+# Setting up
+setup(
+    name="Neutron-Web",
+    version='0.2.0',
+    author="IanTerzo (Ian Baldelli)",
+    author_email="ian.baldelli@gmail.com",
+    description="Create modern cross-platform apps in Python using HTML and CSS",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=['Neutron'],
+    install_requires=['pywebview', 'bs4', 'keyboard'],
+    keywords=['python', 'HTML', 'CSS', 'GUI', 'desktop apps'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
