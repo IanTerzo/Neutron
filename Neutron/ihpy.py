fea2b83e9ac3d9d625ee5a289c0cc88a9fb2102d
Ian Baldelli <79420376+IanTerzo@users.noreply.github.com>
2022-02-06 21:05:24 +0100

    Create compile() function

--- Neutron/ihpy.py
+++ Neutron/ihpy.py
@@ -1,1 +1,35 @@
+import inspect
+
+"""
+In-Html-Python
+
+Example usage:
+{python
+         # The return statement works as in a function except that the return(s) get added to the HTML, works with strings, lists, and tuples.
+         
+         return "This will be displayed in the HTML", Neutron2.elements.H2(win, content="This too")
+
+}
+
+"""
+def compile(html, locals):
+    
+
+    user_code = html[html.find("{python") + len("{python"):html.find("}")]
+
+    runner_src = "def runner():\n" + user_code # To prevent any IndentationErrors and make the return statement work
+
+    exec(runner_src, locals) # Define function
+    result = eval("runner()", locals)
+
+    in_python_html = html[html.find("{python"):html.find("}") + 1]
+
+    if type(result) == tuple or type(result) == list:
+        results_to_str = [str(i) for i in result]
+        results_combined = "\n".join(results_to_str)
+        
+        return html.replace(in_python_html, results_combined)
+        
+    else:
+        return html.replace(in_python_html, str(result))
 
