4261858e1363c2596b4ba390bfc0507151a8027f
Ian Baldelli <79420376+IanTerzo@users.noreply.github.com>
2022-02-11 20:31:25 +0100

    HTML compile error fix

--- Neutron/ihpy.py
+++ Neutron/ihpy.py
@@ -14,22 +14,26 @@ Example usage:
 """
 def compile(html, locals):
     
-
-    user_code = html[html.find("{python") + len("{python"):html.find("}")]
-
-    runner_src = "def runner():\n" + user_code # To prevent any IndentationErrors and make the return statement work
-
-    exec(runner_src, locals) # Define function
-    result = eval("runner()", locals)
-
-    in_python_html = html[html.find("{python"):html.find("}") + 1]
-
-    if type(result) == tuple or type(result) == list:
-        results_to_str = [str(i) for i in result]
-        results_combined = "\n".join(results_to_str)
+    if html.find("{python") != -1: # If it exists
         
-        return html.replace(in_python_html, results_combined)
+        user_code = html[html.find("{python") + len("{python"):html.find("}")]
+
+        runner_src = "def runner():\n" + user_code # To prevent any IndentationErrors and make the return statement work
+        print(user_code)
+
+        exec(runner_src, locals) # Define function
+        result = eval("runner()", locals)
+
+        in_python_html = html[html.find("{python"):html.find("}") + 1]
+
+        if type(result) == tuple or type(result) == list:
+           results_to_str = [str(i) for i in result]
+           results_combined = "\n".join(results_to_str)
         
+           return html.replace(in_python_html, results_combined)
+        
+        else:
+           return html.replace(in_python_html, str(result))
     else:
-        return html.replace(in_python_html, str(result))
 
+        return html
