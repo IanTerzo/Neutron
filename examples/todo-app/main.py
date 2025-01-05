e29320a888e3cb691ed1c1d3341691b93925198f
Ian Baldelli <79420376+IanTerzo@users.noreply.github.com>
2025-01-05 02:15:17 +0100

    Updated example
    Accidentally uploaded the template originally.

--- examples/todo-app/main.py
+++ examples/todo-app/main.py
@@ -1,13 +1,42 @@
 import Neutron
 
-win = Neutron.Window("Example", size=(600, 400), css="def.css")
-win.display(file="render.html")
+# All the CSS and HTML in this example is based on https://bbbootstrap.com/snippets/todo-list-jquery-and-font-awesome-icons-77769811
 
+win = Neutron.Window("Example", size=(800, 500), css="def.css")
 
-def onClick():
-    win.getElementById("title").innerHTML = "Hello:" + win.getElementById("inputName").value
+tasks = [0, 1, 2]
 
+def CreateTask(key):
+    if key == "Enter":
+        taskId = len(tasks)
+        tasks.append(taskId)
 
-win.getElementById("submitName").addEventListener("click", Neutron.event(onClick))
+        taskName = win.getElementById("addTask").value
+
+        # Create task element
+
+        taskHtml = f'<li id="task{taskId}"><span onclick="RemoveTask(this.parentNode.id)"><i class="fa fa-trash">X</i></span> {taskName}</li>'
+        win.getElementById("tasks").append(taskHtml)
+
+        # Or you can do it like this...
+
+        """
+        task = win.createElement("li")
+        task.id = f"task{taskId}"
+
+        span = win.createElement("span")
+        span.onclick = "RemoveTask(this.parentNode.id)"
+        span.innerHTML = '<i class="fa fa-trash">X</i>'
+
+        task.appendChild(span)
+        task.append(taskName)
+
+        win.getElementById("tasks").appendChild(task)
+        """
+
+def RemoveTask(taskid):
+    win.getElementById(taskid).remove()
+
+win.display(file="render.html", pyfunctions=[CreateTask, RemoveTask])
 
 win.show()
